5dc3cc87bbd5508505f8358598e221346429d999
Can Okay <canokay2014@gmail.com>
2018-03-21 12:04:32 +0300

    Ceating styles for components
    Ceating styles view and text header.js

---
+++ 03-Creating-Styles-for-Components/ornek/src/components/Header.js
@@ -0,0 +1,32 @@
+  import React from 'react';
+  import {
+    Text,
+    View
+  } from 'react-native';
+
+  const Header = () => {
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}> Anasayfa </Text>
+    </View>
+  );
+};
+
+  const styles = {
+    textStyle: {
+      fontSize: 20
+    }
+
+    viewStyle: {
+      backgroundColor: '#f8f8f8',
+      height: 60,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: 15,
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+    }
+  };
+
+  export default Header;
