a02417ea85bd89002962d52c9a51057ecf76ef78
Can Okay <canokay2014@gmail.com>
2018-03-21 10:32:23 +0300

    Add a Header Component
    Header.js to App.js

---
+++ HeaderBileseni/ornek/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+// import bölümü
+import React, { Component } from 'react';
+import {
+  Text,
+  AppRegistry
+} from 'react-native';
+import Header from './src/components/Header';
+
+// Component bölümü
+const App = () => (
+    <Header  />
+);
+
+// render bölümü
+AppRegistry.registerComponent('ornekproje', () => App);
